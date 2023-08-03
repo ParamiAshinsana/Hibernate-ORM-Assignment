ccd255aa82bef6646eff150c04b50b89a0373e78
Parami <paramiashinsana@gmail.com>
2023-08-03 16:11:30 +0530

    update the Book entity and the Author entity

--- Part_02_one-to-many/src/main/java/org/example/entity/Author.java
+++ Part_02_one-to-many/src/main/java/org/example/entity/Author.java
@@ -1,4 +1,57 @@
 package org.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Author {
+    @Id
+    private String author_id;
+    private String author_name;
+
+    @ManyToOne
+    private Book book;
+
+    public Author() {
+    }
+
+    public Author(String author_id, String author_name, Book book) {
+        this.author_id = author_id;
+        this.author_name = author_name;
+        this.book = book;
+    }
+
+    public String getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(String author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "author_id='" + author_id + '\'' +
+                ", author_name='" + author_name + '\'' +
+                ", book=" + book +
+                '}';
+    }
 }
--- Part_02_one-to-many/src/main/java/org/example/entity/Book.java
+++ Part_02_one-to-many/src/main/java/org/example/entity/Book.java
@@ -1,4 +1,70 @@
 package org.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Book {
+    @Id
+    private String id;
+    private String ISBN;
+    private String title;
+
+    @OneToMany
+    private List<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String id, String ISBN, String title, List<Author> authors) {
+        this.id = id;
+        this.ISBN = ISBN;
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
